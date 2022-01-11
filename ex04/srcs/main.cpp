f041c60fe24c070dbb16305dc76abe65443080d2
Takashi Nishina <tnishina@c1r22s2.42tokyo.jp>
2022-01-11 17:26:45 +0900

    update main.cpp and add first test case

--- ex04/srcs/main.cpp
+++ ex04/srcs/main.cpp
@@ -10,12 +10,24 @@ int
     if (ifs.fail())
         std::cout << "Failed to open the input file" << std::endl;
     std::string     filename = av[1];
-    filename += ".replace"
+    filename += ".replace";
     std::ofstream   ofs(filename);
     if (ofs.fail())
         std::cout << "Failed to open the output file" << std::endl;
     std::string line;
+	std::string	target = av[2];
+	std::string::size_type	len_of_target = target.length();
+	std::string	replacement = av[3];
     while (std::getline(ifs, line))
-        ofs << line;
+	{
+		std::string::size_type	found = line.find(target);
+		while (found != std::string::npos)
+		{
+			line.erase(found, len_of_target);
+			line.insert(found, replacement);
+			found = line.find(target);
+		}
+        ofs << line << std::endl;
+	}
     return (0);
 }
