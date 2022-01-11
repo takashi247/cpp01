c97b62147520f5554ebe84a84360626436d37622
takashi247 <takashi247@gmail.com>
2022-01-11 18:57:01 +0900

    fix functional pointer handling again

--- ex05/srcs/Karen.cpp
+++ ex05/srcs/Karen.cpp
@@ -1,14 +1,14 @@
 #include "Karen.hpp"
 #include <iostream>
 
-const std::string   DEBUG_MSG = "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!";
-const std::string   INFO_MSG = "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!";
-const std::string   WARNING_MSG = "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.";
-const std::string   ERROR_MSG = "This is unacceptable, I want to speak to the manager now.";
-const int           DEBUG_INDEX = 0;
-const int           INFO_INDEX = 1;
-const int           WARNING_INDEX = 2;
-const int           ERROR_INDEX = 3;
+const std::string   Karen::DEBUG_MSG = "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!";
+const std::string   Karen::INFO_MSG = "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!";
+const std::string   Karen::WARNING_MSG = "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.";
+const std::string   Karen::ERROR_MSG = "This is unacceptable, I want to speak to the manager now.";
+const int           Karen::DEBUG_INDEX = 0;
+const int           Karen::INFO_INDEX = 1;
+const int           Karen::WARNING_INDEX = 2;
+const int           Karen::ERROR_INDEX = 3;
 
 Karen::Karen(){};
 
@@ -38,7 +38,7 @@ void
 	std::cout << ERROR_MSG << std::endl;
 }
 
-void (*const functionArray[])(void) = {
+void Karen::(*const functionArray[])(void) = {
 	Karen::debug,
 	Karen::info,
 	Karen::warning,
@@ -50,5 +50,5 @@ void
 {
 	int	index = (level == "DEBUG") * DEBUG_INDEX + (level == "INFO") * INFO_INDEX \
         + (level == "WARNING") * WARNING_INDEX + (level == "ERROR") * ERROR_INDEX;
-    functionArray[index]();
+    this->functionArray[index]();
 }
