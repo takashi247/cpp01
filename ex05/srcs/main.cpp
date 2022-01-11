35012eae4e909e21a2054db627abb2db0dc836de
takashi247 <takashi247@gmail.com>
2022-01-11 18:37:13 +0900

    fix spelling misses in main.cpp

--- ex05/srcs/main.cpp
+++ ex05/srcs/main.cpp
@@ -6,8 +6,8 @@ int
 {
     Karen   karen;
     karen.complain("DEBUG");
-    karen.compalin("INFO");
-    karen.compalin("WARNING");
-    karen.compalin("ERROR");
-    karen.compalin("HOGE");
+    karen.complain("INFO");
+    karen.complain("WARNING");
+    karen.complain("ERROR");
+    karen.complain("HOGE");
 }
