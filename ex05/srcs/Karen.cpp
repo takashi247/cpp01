7a26b9e36e076f50b4562818efcb43436af571dc
takashi247 <takashi247@gmail.com>
2022-01-11 23:57:55 +0900

    add new lines in Karen's messages in ex05

--- ex05/srcs/Karen.cpp
+++ ex05/srcs/Karen.cpp
@@ -1,9 +1,9 @@
 #include "Karen.hpp"
 #include <iostream>
 
-const std::string   Karen::DEBUG_MSG = "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!";
-const std::string   Karen::INFO_MSG = "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!";
-const std::string   Karen::WARNING_MSG = "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.";
+const std::string   Karen::DEBUG_MSG = "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\nI just love it!";
+const std::string   Karen::INFO_MSG = "I cannot believe adding extra bacon cost more money.\nYou don’t put enough!\nIf you did I would not have to ask for it!";
+const std::string   Karen::WARNING_MSG = "I think I deserve to have some extra bacon for free.\nI’ve been coming here for years and you just started working here last month.";
 const std::string   Karen::ERROR_MSG = "This is unacceptable, I want to speak to the manager now.";
 
 Karen::Karen()
