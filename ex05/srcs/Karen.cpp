b3a2bdafb3706ab6f536f56432c49b37f7264758
takashi247 <takashi247@gmail.com>
2022-01-11 18:43:58 +0900

    add static variable definitions in Karen.cpp

--- ex05/srcs/Karen.cpp
+++ ex05/srcs/Karen.cpp
@@ -1,6 +1,15 @@
 #include "Karen.hpp"
 #include <iostream>
 
+const std::string   DEBUG_MSG = "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!";
+const std::string   INFO_MSG = "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!";
+const std::string   WARNING_MSG = "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.";
+const std::string   ERROR_MSG = "This is unacceptable, I want to speak to the manager now.";
+const int           DEBUG_INDEX = 0;
+const int           INFO_INDEX = 1;
+const int           WARNING_INDEX = 2;
+const int           ERROR_INDEX = 3;
+
 Karen::Karen();
 
 Karen::~Karen();
