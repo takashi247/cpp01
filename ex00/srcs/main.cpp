26253975eefb088c2bedd6c091571b0cfb0ffa71
takashi247 <takashi247@gmail.com>
2022-01-12 15:53:56 +0900

    fix main functions

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,8 +6,8 @@ int
 	Zombie	z1("zombie 1");
 	z1.announce();
 	Zombie	*z2;
-	z2 = z1.newZombie("zombie 2");
+	z2 = newZombie("zombie 2");
 	z2->announce();
 	delete z2;
-	z1.randomChump("zombie 3");
+	randomChump("zombie 3");
 }
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -3,11 +3,8 @@
 int
 	main()
 {
-	Zombie	z1("zombie 1");
-	z1.announce();
-	Zombie	*zombies;
 	const int	num_of_zombies = 5;
-	zombies = z1.zombieHorde(num_of_zombies, "hoge");
+	zombies = zombieHorde(num_of_zombies, "hoge");
 	for (int i = 0; i < num_of_zombies; i++)
 		zombies[i].announce();
 	delete [] zombies;
